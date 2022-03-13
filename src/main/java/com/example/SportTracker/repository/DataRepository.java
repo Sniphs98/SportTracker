7fa81cfa620a70bb6ed911a16a738ba37039842e
Sniphs <lukas@bronstering.com>
2022-03-13 14:04:05 +0100

    first commit

---
+++ src/main/java/com/example/SportTracker/repository/DataRepository.java
@@ -0,0 +1,7 @@
+package com.example.SportTracker.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataRepository extends CrudRepository<> {
+}
---
+++ src/main/java/com/example/SportTracker/service/DataService.java
@@ -0,0 +1,12 @@
+package com.example.SportTracker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+
+    public String sayHallo(){
+        return "hallo";
+    }
+
+}
